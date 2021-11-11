cd9b0ef05822b9dfb4ea3b6eef7d55da6490b56b
Adam Sparks <adamsparks92@gmail.com>
2021-11-11 12:06:09 -0600

    Update server.js
    here we call init and shim fetch so we're ready to render upon request
    
    fixes #3

--- files/server.js
+++ files/server.js
@@ -1,46 +1,54 @@
-import path from "path";
-import { fileURLToPath, URL, URLSearchParams } from "url";
-import { get_body as getBody } from "@sveltejs/app-utils/http"; // eslint-disable-line node/file-extension-in-import
+import path from 'path';
+import { fileURLToPath, URL, URLSearchParams } from 'url';
+import { get_body as getBody } from '@sveltejs/app-utils/http'; // eslint-disable-line node/file-extension-in-import
+import { fetch, Response, Request, Headers } from '@sveltejs/kit/install-fetch';
+import { render, init } from './app.js';
 
-import express from "express";
-import compression from "compression";
+import express from 'express';
+import compression from 'compression';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const {
-  PORT = 3000,
-  ASSETS = path.join(__dirname, "assets"),
-  PRERENDERED = path.join(__dirname, "prerendered"),
+	PORT = 3000,
+	ASSETS = path.join(__dirname, 'assets'),
+	PRERENDERED = path.join(__dirname, 'prerendered')
 } = process.env;
 
+// shim fetch for node
+global.fetch = fetch;
+global.Response = Response;
+global.Request = Request;
+global.Headers = Headers;
+
+init();
+
 const svelteKit = async (request, response) => {
-  const host = `${request.headers["x-forwarded-proto"]}://${request.headers.host}`;
-  const { pathname, query = "" } = new URL(request.url || "", host);
+	const host = `${request.headers['x-forwarded-proto']}://${request.headers.host}`;
+	const { pathname, query = '' } = new URL(request.url || '', host);
 
-  const { render } = await import("./app.js");
+	const rendered = await render({
+		host,
+		method: request.method,
+		headers: request.headers,
+		path: pathname,
+		query: new URLSearchParams(query),
+		body: await getBody(request)
+	});
 
-  const rendered = await render({
-    host,
-    method: request.method,
-    headers: request.headers,
-    path: pathname,
-    query: new URLSearchParams(query),
-    body: await getBody(request),
-  });
+	if (rendered) {
+		const { status, headers, body } = rendered;
+		return response.writeHead(status, headers).end(body);
+	}
 
-  if (rendered) {
-    const { status, headers, body } = rendered;
-    return response.writeHead(status, headers).end(body);
-  }
-
-  return response.writeHead(404).end();
+	return response.writeHead(404).end();
 };
 
 const app = express();
 app.use(compression());
-app.use("/", express.static(ASSETS));
-app.use("/", express.static(PRERENDERED));
+app.use('/', express.static(ASSETS));
+app.use('/', express.static(PRERENDERED));
 app.use(svelteKit);
 
 app.listen(PORT);
